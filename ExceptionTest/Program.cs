38fc28706d733e8b59147f1100a2fceb592e1b3a
Pedro Ferreira <pp.ferreira2300@gmail.com>
2021-10-26 15:10:54 +0100

    Using Exception message

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -6,9 +6,18 @@ namespace ExceptionTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insere um inteiro");
-            int i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Numero Inserido : {i}");
+           try
+           {
+                Console.WriteLine("Insere um inteiro");
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Numero Inserido : {i}");
+           } 
+           catch(Exception e)
+           {
+                throw new Exception($"Error => {e.Message}");
+           }
+           
+
         }   
     }
 }
